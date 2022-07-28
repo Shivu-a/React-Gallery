917b418263a222731b4750409f0a935db6c7b087
Shivu-a <sebaingame@gmail.com>
2022-07-28 05:18:41 -0300

    Added PopUp

---
+++ src/components/PopUpImage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const PopUpImage = ({ source, viewImage }) => {
+  return (
+    <div className="flex justify-center bg-black bg-opacity-90 fixed top-0 left-0 w-full h-screen">
+      <button
+        onClick={viewImage}
+        className="fixed right-2 top-0 text-white text-xl p-6"
+      >
+        X
+      </button>
+      <img className="max-h-screen max-w-full" src={source} />
+    </div>
+  );
+};
